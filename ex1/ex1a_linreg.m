b053e4e50f2f690e77ff56856c76e9f2d0dac569
Jeremy Spykerman <jeremy.spykerman@gmail.com>
2016-04-18 20:56:18 +0100

    missed this in the last commit

--- ex1/ex1a_linreg.m
+++ ex1/ex1a_linreg.m
@@ -42,10 +42,10 @@ theta = rand(n,1);
 % TODO:  Implement the linear regression objective and gradient computations
 % in linear_regression.m
 %
-tic;
+%tic;
 options = struct('MaxIter', 200);
-theta = minFunc(@linear_regression, theta, options, train.X, train.y);
-fprintf('Optimization took %f seconds.\n', toc);
+%theta = minFunc(@linear_regression, theta, options, train.X, train.y);
+%fprintf('Optimization took %f seconds.\n', toc);
 
 % Run minFunc with linear_regression_vec.m as the objective.
 %
@@ -56,10 +56,10 @@ fprintf('Optimization took %f seconds.\n', toc);
 %
 % Uncomment the lines below to run your vectorized code.
 %Re-initialize parameters
-%theta = rand(n,1);
-%tic;
-%theta = minFunc(@linear_regression_vec, theta, options, train.X, train.y);
-%fprintf('Optimization took %f seconds.\n', toc);
+theta = rand(n,1);
+tic;
+theta = minFunc(@linear_regression_vec, theta, options, train.X, train.y);
+fprintf('Optimization took %f seconds.\n', toc);
 
 % Plot predicted prices and actual prices from training set.
 actual_prices = train.y;
@@ -88,3 +88,4 @@ if (plot_prices)
   xlabel('House #');
   ylabel('House price ($1000s)');
 end
+pause(1);
